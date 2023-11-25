142d4907c85dfe90ec98a0df990b7e0d0886ff6c
Darsh <pareekdarsh0162@gmail.com>
2023-11-26 03:13:44 +0530

    final frontend

--- data_gen.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-data = pd.read_csv('sudoku.csv')
-X_s = np.array(data['quizzes'])
-Y_s = np.array(data['solutions'])
-for i in range(len(X_s)):
-    X_s[i] = np.array(list(X_s[i]),'int32').reshape(9,9)
-    Y_s[i] = np.array(list(Y_s[i]),'int32').reshape(9,9)
